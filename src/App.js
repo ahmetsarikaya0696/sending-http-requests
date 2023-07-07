6b55da3d354d1b0f9b1cfdc1d99f18510e7fc6bf
Ahmet Sarıkaya <ahmetsarikaya0696@outlook.com>
2023-07-07 20:32:05 +0300

    Add project files.

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+
+  function fetchMoviesHandler() {
+    fetch("https://swapi.dev/api/films/")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const transformedMovies = data.results.map((result) => {
+          return {
+            id: result.episode_id,
+            title: result.title,
+            openingText: result.opening_crawl,
+            releaseDate: result.release_date,
+          };
+        });
+        setMovies(transformedMovies);
+      });
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
